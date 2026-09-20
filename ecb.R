41aa67a80daf3b112b03b705228ad7ef84552f18
Luís Fonseca <luispfonseca@gmail.com>
2026-09-20 14:58:19 +0200

    Rewrite ecb.R in base R, dropping tidyverse dependencies
    Only xml2 remains (to fetch and parse the ECB page); everything else
    now uses base R. Output is byte-identical to the previous version.

--- ecb.R
+++ ecb.R
@@ -1,45 +1,42 @@
-library(rvest)
-library(magrittr)
-library(dplyr)
-library(tidyr)
-library(stringr)
-library(purrr)
+# Scrape the ECB Governing Council calendar and write .ics feeds.
+# Base R only, except xml2 to fetch and parse the HTML page.
+library(xml2)
 
 url <- "https://www.ecb.europa.eu/press/calendars/mgcgc/html/index.en.html"
 
-events <- 
-  read_html(url) %>% 
-  html_node(".definition-list") %>% 
-  html_text() %>% 
-  str_split("\\t\\n") %>% 
-  .[[1]] %>%
-  str_subset("^ $", T) %>% 
-  str_subset("^\n $", T) %>% 
-  str_subset("^$", T) %>% 
-  str_split("\\n\\n\\s+\\n") %>% 
-  tibble::enframe() %>% 
-  unnest(value) %>% 
-  group_by(name) %>% 
-  mutate(type = if_else(row_number() == 1, "date", "detail")) %>% 
-  ungroup() %>% 
-  pivot_wider(names_from = type, values_from = value) %>% 
-  mutate(across(detail, ~str_remove_all(., "\\t") %>% str_trim())) %>% 
-  mutate(date = lubridate::dmy(date)) %>% 
-  select(-name) %>% 
-  # delete the press conferences after the gov council meeting
-  filter(str_detect(detail, "Press conference following the Governing Council meeting", T)) %>% 
-  mutate(across(detail, \(x) str_replace(x, "Governing Council of the ECB: ", "ECB GovC: "))) %>%
-  mutate(across(detail, \(x) str_replace(x, "General Council meeting of the ECB", "ECB General Council: meeting")))
+# grab the text of the calendar's definition-list (CSS ".definition-list")
+node <- xml_find_first(
+  read_html(url),
+  "//*[contains(concat(' ', normalize-space(@class), ' '), ' definition-list ')]"
+)
+raw <- xml_text(node)
 
-if (!length(events$date %>% unique()) == nrow(events)) {
+# split into entries and drop the blank separators between them
+entries <- strsplit(raw, "\t\n")[[1]]
+entries <- entries[nzchar(trimws(entries))]
+
+# each entry is "<date>\n\n   \n<detail>\t   "; split into the two halves
+parts  <- strsplit(entries, "\n\n[[:space:]]+\n")
+date   <- as.Date(vapply(parts, `[`, character(1), 1L), format = "%d/%m/%Y")
+detail <- trimws(gsub("\t", "", vapply(parts, `[`, character(1), 2L)))
+
+events <- data.frame(date = date, detail = detail)
+
+# delete the press conferences after the gov council meeting
+events <- events[!grepl("Press conference following the Governing Council meeting", events$detail), ]
+
+# shorten the wording
+events$detail <- sub("Governing Council of the ECB: ", "ECB GovC: ", events$detail)
+events$detail <- sub("General Council meeting of the ECB", "ECB General Council: meeting", events$detail)
+
+if (length(unique(events$date)) != nrow(events)) {
   stop("More than one event in the same day; check to see if these can be merged.\nThis is because code is assuming only one event per day to generate the UIDs")
 }
 
 make_event <- function(date, summary) {
-  #output <- 
-    c(
+  c(
     "BEGIN:VEVENT",
-    paste0("DTEND;VALUE=DATE:", gsub("-", "", date+1)),
+    paste0("DTEND;VALUE=DATE:", gsub("-", "", date + 1)),
     paste0("DTSTART;VALUE=DATE:", gsub("-", "", date)),
     paste0("SUMMARY:", summary),
     paste0("UID:ca6af7456b0088abad9a69f9f620f5ac-", date),
@@ -50,30 +47,21 @@ make_event <- function(date, summary) {
 }
 
 write_file <- function(events, path) {
-  c(
+  lines <- c(
     "BEGIN:VCALENDAR",
     "VERSION:2.0",
     "METHOD:PUBLISH",
     "PRODID:PERSONALCALENDAR",
-    "CALSCALE:GREGORIAN"
-  ) %>% 
-    cat(file = file.path(path), sep = "\n")
-  
-  # add the events
-  events %>% 
-    mutate(event = map2(date, detail, make_event)) %>% 
-    select(event) %>% 
-    unnest(event) %>% 
-    pull(event) %>% 
-    cat(file = file.path(path), sep="\n", append = TRUE)
-  
-  # end the file
-  cat(c("END:VCALENDAR"), file = file.path(path), sep="\n", append = TRUE)
+    "CALSCALE:GREGORIAN",
+    unlist(Map(make_event, events$date, events$detail)),
+    "END:VCALENDAR"
+  )
+  writeLines(lines, path)
 }
 
 # write two versions: all and only monetary policy
 write_file(events, "docs/ecb_calendar.ics")
 
-events %>% 
-  filter(str_detect(detail, regex("monetary", ignore_case = TRUE)) & str_detect(detail, regex("non-monetary", ignore_case = TRUE), TRUE)) %>% 
-  write_file("docs/ecb_calendar_mon_policy.ics")
+mon_policy <- events[grepl("monetary", events$detail, ignore.case = TRUE) &
+                       !grepl("non-monetary", events$detail, ignore.case = TRUE), ]
+write_file(mon_policy, "docs/ecb_calendar_mon_policy.ics")
